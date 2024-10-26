dd5ee28fad5a4c276e16e591b6c8c7ecad3f9d14
Phat <2251120435@ut.edu.vn>
2024-10-26 18:01:21 +0700

    Fix container

--- home.php
+++ home.php
@@ -2,169 +2,149 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="assets/css/home.css">
-    <link rel="stylesheet" href="assets/fonts/fontawesome-free-6.6.0-web/css/all.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="assets/css/home.css">
+  <link rel="stylesheet" href="assets/css/header.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 </head>
 
 <body>
-    <div class="center">
-        <div class="container">
-            <div class="lst-img">
-                <img src="assets/images/logo/asus.png" alt="HP">
-                <img src="assets/images/logo/dell.png" alt="Asus">
-                <img src="assets/images/logo/hp.png" alt="Dell">
-                <img src="assets/images/logo/lenovo.png" alt="Lenovo">
-                <img src="assets/images/logo/macbook.png" alt="Macbook">
-                <img src="assets/images/logo/acer.png" alt="Acer">
-            </div>
-            <div class="filter-section_1">
-                <div class="filter-section">
-                    <h3>Giá</h3>
-                    <select name="price">
-                        <option>none</option>
-                        <option>Dưới 10 triệu</option>
-                        <option>15 - 20 triệu</option>
-                        <option>10 - 15 triệu</option>
-                        <option>20 - 25 triệu</option>
-                        <option>25 - 30 triệu</option>
-                        <option>Trên 30 triệu</option>
-                    </select>
-                </div>
+  <?php
+  include('includes/header.php');
+  ?>
 
-                <div class="filter-section">
-                    <h3>Loại sản phẩm</h3>
-                    <select name="type">
-                        <option>none</option>
-                        <option>Laptop AI</option>
-                        <option>Gaming</option>
-                        <option>Học tập, văn phòng</option>
-                        <option>Đồ họa</option>
-                        <option>Kỹ thuật</option>
-                        <option>Mỏng nhẹ</option>
-                        <option>Cao cấp</option>
-                    </select>
-                </div>
-
-                <div class="filter-section">
-                    <h3>RAM</h3>
-                    <select name="ram">
-                        <option>none</option>
-                        <option>2 GB</option>
-                        <option>4 GB</option>
-                        <option>8 GB</option>
-                        <option>16 GB</option>
-                        <option>18 GB</option>
-                        <option>24 GB</option>
-                    </select>
-                </div>
-
-                <div class="filter-section">
-                    <h3>Ổ cứng</h3>
-                    <select name="disk">
-                        <option>none</option>
-                        <option>SSD 2TB</option>
-                        <option>SSD 1TB</option>
-                        <option>SSD 512GB</option>
-                        <option>SSD 256GB</option>
-                    </select>
-                </div>
-                <button id="filterButton"><i class="fa-solid fa-filter"></i>&nbspLọc</button>
-            </div>
-
-            <div class="product">
-                <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                    alt="MacBook Air 15 inch M3">
-                <h3>MacBook Air 15 inch M31</h3>
-                <p class="price">36.690.000₫</p>
-                <p class="discount">37.990.000₫</p>
-                <button>Mua ngay</button>
-            </div>
-            <div class="product">
-                <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                    alt="MacBook Air 15 inch M3">
-                <h3>MacBook Air 15 inch M31</h3>
-                <p class="price">36.690.000₫</p>
-                <p class="discount">37.990.000₫</p>
-                <button>Mua ngay</button>
-            </div>
-            <div class="product">
-                <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                    alt="MacBook Air 15 inch M3">
-                <h3>MacBook Air 15 inch M31</h3>
-                <p class="price">36.690.000₫</p>
-                <p class="discount">37.990.000₫</p>
-                <button>Mua ngay</button>
-            </div>
-            <div class="product">
-                <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                    alt="MacBook Air 15 inch M3">
-                <h3>MacBook Air 15 inch M31</h3>
-                <p class="price">36.690.000₫</p>
-                <p class="discount">37.990.000₫</p>
-                <button>Mua ngay</button>
-            </div>
-            <div class="product">
-                <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                    alt="MacBook Air 15 inch M3">
-                <h3>MacBook Air 15 inch M31</h3>
-                <p class="price">36.690.000₫</p>
-                <p class="discount">37.990.000₫</p>
-                <button>Mua ngay</button>
-            </div>
-            <div class="product">
-                <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                    alt="MacBook Air 15 inch M3">
-                <h3>MacBook Air 15 inch M31</h3>
-                <p class="price">36.690.000₫</p>
-                <p class="discount">37.990.000₫</p>
-                <button>Mua ngay</button>
-            </div>
-            <div class="product">
-                <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                    alt="MacBook Air 15 inch M3">
-                <h3>MacBook Air 15 inch M31</h3>
-                <p class="price">36.690.000₫</p>
-                <p class="discount">37.990.000₫</p>
-                <button>Mua ngay</button>
-            </div>
-            <div class="product">
-                <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                    alt="MacBook Air 15 inch M3">
-                <h3>MacBook Air 15 inch M31</h3>
-                <p class="price">36.690.000₫</p>
-                <p class="discount">37.990.000₫</p>
-                <button>Mua ngay</button>
-            </div>
-            <div class="product">
-                <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                    alt="MacBook Air 15 inch M3">
-                <h3>MacBook Air 15 inch M31</h3>
-                <p class="price">36.690.000₫</p>
-                <p class="discount">37.990.000₫</p>
-                <button>Mua ngay</button>
-            </div>
-            <div class="product">
-                <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                    alt="MacBook Air 15 inch M3">
-                <h3>MacBook Air 15 inch M31</h3>
-                <p class="price">36.690.000₫</p>
-                <p class="discount">37.990.000₫</p>
-                <button>Mua ngay</button>
-            </div>
-            <div class="product">
-                <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                    alt="MacBook Air 15 inch M3">
-                <h3>MacBook Air 15 inch M31</h3>
-                <p class="price">36.690.000₫</p>
-                <p class="discount">37.990.000₫</p>
-                <button>Mua ngay</button>
-            </div>
+    <div class="container">
+      <div class="lst-img">
+        <img src="assets/images/logo/asus.png" alt="HP">
+        <img src="assets/images/logo/dell.png" alt="Asus">
+        <img src="assets/images/logo/hp.png" alt="Dell">
+        <img src="assets/images/logo/lenovo.png" alt="Lenovo">
+        <img src="assets/images/logo/macbook.png" alt="Macbook">
+        <img src="assets/images/logo/acer.png" alt="Acer">
+        <img src="assets/images/logo/msi.png" alt="MSI">
+      </div>
+      <div class="filter-section">
+        <div>
+          <h3>Giá: 
+            <select name="price">
+              <option>none</option>
+              <option>Dưới 10 triệu</option>
+              <option>15 - 20 triệu</option>
+              <option>10 - 15 triệu</option>
+              <option>20 - 25 triệu</option>
+              <option>25 - 30 triệu</option>
+              <option>Trên 30 triệu</option>
+            </select>
+          </h3>
         </div>
-    </div>
-    </div>
+
+        <div>
+          <h3>Loại sản phẩm: 
+            <select name="type">
+              <option>none</option>
+              <option>Laptop AI</option>
+              <option>Gaming</option>
+              <option>Học tập, văn phòng</option>
+              <option>Đồ họa</option>
+              <option>Kỹ thuật</option>
+              <option>Mỏng nhẹ</option>
+              <option>Cao cấp</option>
+            </select>
+          </h3>
+        </div>
+
+        <div>
+          <h3>RAM: 
+            <select name="ram">
+              <option>none</option>
+              <option>2 GB</option>
+              <option>4 GB</option>
+              <option>8 GB</option>
+              <option>16 GB</option>
+              <option>18 GB</option>
+              <option>24 GB</option>
+            </select>
+          </h3>
+        </div>
+
+        <div>
+          <h3>Ổ cứng: 
+            <select name="disk">
+              <option>none</option>
+              <option>SSD 2TB</option>
+              <option>SSD 1TB</option>
+              <option>SSD 512GB</option>
+              <option>SSD 256GB</option>
+            </select>
+          </h3>
+        </div>
+        <button id="filterButton"><i class="fa-solid fa-filter"></i>&nbspLọc</button>
+      </div>
+      
+      <div class="product">
+        <img src="https://cdn.tgdd.vn/Products/Images/44/313333/lenovo-ideapad-slim-3-15iah8-i5-83er00evn-thumb-600x600.jpg"
+          alt="Laptop Lenovo Ideapad Slim 3 15IAH8 i5 12450H/16GB/512GB/Win11 (83ER000EVN)">
+        <h3>Laptop Lenovo Ideapad Slim 3 15IAH8 i5 12450H/16GB/512GB/Win11 (83ER000EVN)</h3>
+        <p class="price">36.690.000₫</p>
+        <p class="discount">37.990.000₫</p>
+        <div class="button-container">
+          <button>Mua ngay</button>
+        </div>
+      </div>
+      <div class="product">
+        <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
+          alt="MacBook Air 15 inch M3">
+        <h3>MacBook Air 15 inch M3</h3>
+        <p class="price">36.690.000₫</p>
+        <p class="discount">37.990.000₫</p>
+        <div class="button-container">
+          <button>Mua ngay</button>
+        </div>
+      </div>
+      <div class="product">
+        <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
+          alt="MacBook Air 15 inch M3">
+        <h3>MacBook Air 15 inch M3</h3>
+        <p class="price">36.690.000₫</p>
+        <p class="discount">37.990.000₫</p>
+        <div class="button-container">
+          <button>Mua ngay</button>
+        </div>
+      </div>
+      <div class="product">
+        <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
+          alt="MacBook Air 15 inch M3">
+        <h3>MacBook Air 15 inch M3</h3>
+        <p class="price">36.690.000₫</p>
+        <p class="discount">37.990.000₫</p>
+        <div class="button-container">
+          <button>Mua ngay</button>
+        </div>
+      </div>
+      <div class="product">
+        <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
+          alt="MacBook Air 15 inch M3">
+        <h3>MacBook Air 15 inch M3</h3>
+        <p class="price">36.690.000₫</p>
+        <p class="discount">37.990.000₫</p>
+        <div class="button-container">
+          <button>Mua ngay</button>
+        </div>
+      </div>
+      <div class="product">
+        <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
+          alt="MacBook Air 15 inch M3">
+        <h3>MacBook Air 15 inch M3</h3>
+        <p class="price">36.690.000₫</p>
+        <p class="discount">37.990.000₫</p>
+        <div class="button-container">
+          <button>Mua ngay</button>
+        </div>
+      </div>
+      
+    </div> 
 </body>
 
 </html>
