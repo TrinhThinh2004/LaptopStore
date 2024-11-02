7a4a622deb722f4150047dfd65c06c4443aa3701
lamtriphucUTH <2251120436@ut.edu.vn>
2024-11-02 19:08:35 +0700

    data and product_detail link

--- data/LaptopData.sql
+++ data/LaptopData.sql
@@ -10,7 +10,7 @@ VALUES
 
 ('Acer', 'acer_predator_helios_neo_phn16_71_53m7_i5', 'Intel Core i5 Raptor Lake - 13500HX', 16, 'DDR4', '4800 MHz', 512, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4060 (8GB)', '16 inches', 'Full HD (1920 x 1080)', '165Hz', 25990000, 'Laptop Acer Predator Helios Neo PHN16 71 53M7 i5 13500HX/16GB/512GB/8GB RTX4060/165Hz/Win11 (NH.QLUSV.005)'),
 
-('Acer', 'acer_swift_go_14_41_r251_r5', 'AMD Ryzen 5 - 7430U', 16, 'DDR4', '4800 MHz', 1000, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '14 inches', 'Full HD (1920 x 1080)', '60Hz', 16490000, 'Laptop Acer Swift Go 14 41 R251 R5 7430U/16GB/1TB/Win11 (NX.KG3SV.005)'),
+('Acer', 'acer_swift_go_14_41_r251_r5', 'AMD Ryzen 5 - 7430U', 16, 'DDR4', '4800 MHz', 1024, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '14 inches', 'Full HD (1920 x 1080)', '60Hz', 16490000, 'Laptop Acer Swift Go 14 41 R251 R5 7430U/16GB/1TB/Win11 (NX.KG3SV.005)'),
 
 ('Acer', 'acer_swift_lite_14_al_sfl14_51m_56hs_ultra_5', 'Intel Core Ultra 5 Meteor Lake - 125U', 16, 'DDR5', '5600 MHz', 512, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '14 inches', '1920 x 1200', '60Hz', 18190000, 'Laptop Acer Swift Lite 14 AI SFL14 51M 56HS Ultra 5 125U/16GB/512GB/Win11 (NX.J1HSV.002)'),
 
@@ -30,17 +30,17 @@ VALUES
 
 
 
-('Dell', 'dell_g15_5530_i9', 'Intel Core i9 Raptor Lake - 13900HX', 16, 'DDR5', '4800 MHz', 1000, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4060 (8GB)', '15.6 inches', 'Full HD (1920 x 1080)', '165Hz', 40990000, 'Laptop Dell G15 5530 i9 13900HX/16GB/1TB/8GB RTX4060/165Hz/OfficeHS/Win11 (i9HX161W11GR4060)'),
+('Dell', 'dell_g15_5530_i9', 'Intel Core i9 Raptor Lake - 13900HX', 16, 'DDR5', '4800 MHz', 1024, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4060 (8GB)', '15.6 inches', 'Full HD (1920 x 1080)', '165Hz', 40990000, 'Laptop Dell G15 5530 i9 13900HX/16GB/1TB/8GB RTX4060/165Hz/OfficeHS/Win11 (i9HX161W11GR4060)'),
 
 ('Dell', 'dell_inspiron_15_3520_i5', 'Intel Core i5 Alder Lake - 1235U', 16, 'DDR4', '2666 MHz', 512, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '15.6 inches', 'Full HD (1920 x 1080)', '120Hz', 16490000, 'Laptop Dell Inspiron 15 3520 i5 1235U/16GB/512GB/120Hz/OfficeHS/Win11 (N5I5052W1)'),
 
 ('Dell', 'dell_inspiron_15_3530_i7', 'Intel Core i7 Raptor Lake - 1355U', 16, 'DDR4', '3200 MHz', 512, 'SSD NVMe PCIe', 'NVIDIA GeForce MX550 (2GB)', '15.6 inches', 'Full HD (1920 x 1080)', '120Hz', 23990000, 'Laptop Dell Inspiron 15 3530 i7 1355U/16GB/512GB/2GB MX550/OfficeHS/Win11 (N3530I716W1)'),
 
-('Dell', 'dell_precision_15_3581_i7', 'Intel Core i7 Raptor Lake - 13800H', 32, 'DDR5', '5200 MHz', 1000, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX A500 (4GB)', '15.6 inches', 'Full HD (1920 x 1080)', '60Hz', 57290000, 'Laptop Dell Precision 15 3581 i7 13800H/32GB/1TB/4GB RTXA500/Win11 Pro (71024677)'),
+('Dell', 'dell_precision_15_3581_i7', 'Intel Core i7 Raptor Lake - 13800H', 32, 'DDR5', '5200 MHz', 1024, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX A500 (4GB)', '15.6 inches', 'Full HD (1920 x 1080)', '60Hz', 57290000, 'Laptop Dell Precision 15 3581 i7 13800H/32GB/1TB/4GB RTXA500/Win11 Pro (71024677)'),
 
-('Dell', 'dell_xps_13_9340_ultra_5', 'Intel Core Ultra 5 Meteor Lake - 125H', 16, 'DDR5', '7467 MHz', 2000, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '13.4 inches', 'QHD+ (2560 x 1600)', '120Hz', 52990000, 'Laptop Dell XPS 13 9340 Ultra 5 125H/16GB/2TB/Touch/OfficeHS/Win11 (XPSU5934W1)'),
+('Dell', 'dell_xps_13_9340_ultra_5', 'Intel Core Ultra 5 Meteor Lake - 125H', 16, 'DDR5', '7467 MHz', 2048, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '13.4 inches', 'QHD+ (2560 x 1600)', '120Hz', 52990000, 'Laptop Dell XPS 13 9340 Ultra 5 125H/16GB/2TB/Touch/OfficeHS/Win11 (XPSU5934W1)'),
 
-('Dell', 'dell_xps_13_9340_ultra_7', 'Intel Core Ultra 7 Meteor Lake - 155H', 16, 'DDR5', '7467 MHz', 1000, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '13.4 inches', '2560 x 1600', '60Hz', '55990000', 'Laptop Dell XPS 13 9340 Ultra 7 155H/16GB/1TB/Touch/OfficeHS/Win11 (HXRGT)'),
+('Dell', 'dell_xps_13_9340_ultra_7', 'Intel Core Ultra 7 Meteor Lake - 155H', 16, 'DDR5', '7467 MHz', 1024, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '13.4 inches', '2560 x 1600', '60Hz', '55990000', 'Laptop Dell XPS 13 9340 Ultra 7 155H/16GB/1TB/Touch/OfficeHS/Win11 (HXRGT)'),
 
 
 
@@ -52,15 +52,15 @@ VALUES
 
 ('HP', 'hp_gaming_victus_15_fa1139tx_i5', 'Intel Core i5 Alder Lake - 12450H', 16, 'DDR4', '3200 MHz', 512, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 2050 (4GB)', '15.6 inches', 'Full HD (1920 x 1080)', '144Hz', 19490000, 'Laptop HP Gaming VICTUS 15 fa1139TX i5 12450H/16GB/512GB/4GB RTX2050/144Hz/Win11 (8Y6W3PA)'),
 
-('HP', 'hp_omen_16_xf0070ax_r9', 'AMD Ryzen 9 - 7940HS', 32, 'DDR5', '5600 MHz', 1000, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4070 (8GB)', '16.1 inches', 'QHD', '240Hz', 57890000, 'Laptop HP OMEN 16 xf0070AX R9 7940HS/32GB/1TB/8GB RTX4070/240Hz/Win11 (8W945PA)'),
+('HP', 'hp_omen_16_xf0070ax_r9', 'AMD Ryzen 9 - 7940HS', 32, 'DDR5', '5600 MHz', 1024, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4070 (8GB)', '16.1 inches', 'QHD', '240Hz', 57890000, 'Laptop HP OMEN 16 xf0070AX R9 7940HS/32GB/1TB/8GB RTX4070/240Hz/Win11 (8W945PA)'),
 
 ('HP', 'hp_pavilion_15_eg2062tx_i5', 'Intel Core i5 Alder Lake - 1235U', 8, 'DDR4', '3200 MHz', 512, 'SSD NVMe PCIe', 'NVIDIA GeForce MX550 (2GB)', '15.6 inches', 'Full HD (1920 x 1080)', '60Hz', 15790000, 'Laptop HP Pavilion 15 eg2062TX i5 1235U/8GB/512GB/2GB MX550/Win11 (7C0W7PA)'),
 
 
 
-('Lenovo', 'lenovo_gaming_legion_pro_5_16irx9_i9', 'Intel Core i9 Raptor Lake - 14900HX', 32, 'DDR5', '5600 MHz', 1000, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4060 (8GB)', '16 inches', 'WQXGA (2560 x 1600)', '240Hz', 46990000, 'Laptop Lenovo Gaming Legion Pro 5 16IRX9 i9 14900HX/32GB/1TB/8GB RTX4060/240Hz/Win11 (83DF0047VN)'),
+('Lenovo', 'lenovo_gaming_legion_pro_5_16irx9_i9', 'Intel Core i9 Raptor Lake - 14900HX', 32, 'DDR5', '5600 MHz', 1024, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4060 (8GB)', '16 inches', 'WQXGA (2560 x 1600)', '240Hz', 46990000, 'Laptop Lenovo Gaming Legion Pro 5 16IRX9 i9 14900HX/32GB/1TB/8GB RTX4060/240Hz/Win11 (83DF0047VN)'),
 
-('Lenovo', 'lenovo_gaming_legion_slim_5_16ahp9_r7', 'AMD Ryzen 7 - 8845HS', 32, 'DDR5', '5600 MHz', 1000, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4060 (8GB)', '16 inches', 'WQXGA (2560 x 1600)', '165Hz', 42990000, 'Laptop Lenovo Gaming Legion Slim 5 16AHP9 R7 8845HS/32GB/1TB/8GB RTX4060/165Hz/Win11 (83DH003BVN)'),
+('Lenovo', 'lenovo_gaming_legion_slim_5_16ahp9_r7', 'AMD Ryzen 7 - 8845HS', 32, 'DDR5', '5600 MHz', 1024, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4060 (8GB)', '16 inches', 'WQXGA (2560 x 1600)', '165Hz', 42990000, 'Laptop Lenovo Gaming Legion Slim 5 16AHP9 R7 8845HS/32GB/1TB/8GB RTX4060/165Hz/Win11 (83DH003BVN)'),
 
 ('Lenovo', 'lenovo_gaming_loq_15iax9_i5', 'Intel Core i5 Alder Lake - 12450HX', 16, 'DDR4', '4800 MHz', 512, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 3050 (6GB)', '15.6 inches', 'Full HD (1920 x 1080)', '144Hz', 21690000, 'Laptop Lenovo Gaming LOQ 15IAX9 i5 12450HX/16GB/512GB/6GB RTX3050/144Hz/Win11 (83GS000JVN)'),
 
@@ -68,22 +68,22 @@ VALUES
 
 ('Lenovo', 'lenovo_thinkpad_e14_gen_6_ultra_7', 'Intel Core Ultra 7 Meteor Lake - 155U', 16, 'DDR5', '5600 MHz', 512, 'SSD NVMe PCIe', 'Intel Graphics', '14 inches', 'WUXGA', '60Hz', 27990000, 'Laptop Lenovo ThinkPad E14 Gen 6 Ultra 7 155U/16GB/512GB/Win11 (21M7004UVN)'),
 
-('Lenovo', 'lenovo_yoga_slim_7_14imh9_ultra_7', 'Intel Core Ultra 7 Meteor Lake - 155H', 32, 'DDR5', '7467 MHz', 1000, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '14 inches', 'WUXGA, OLED', '60Hz', 30990000, 'Laptop Lenovo Yoga Slim 7 14IMH9 Ultra 7 155H/32GB/1TB/OfficeHS/Win11 (83CV001VVN)'),
+('Lenovo', 'lenovo_yoga_slim_7_14imh9_ultra_7', 'Intel Core Ultra 7 Meteor Lake - 155H', 32, 'DDR5', '7467 MHz', 1024, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '14 inches', 'WUXGA, OLED', '60Hz', 30990000, 'Laptop Lenovo Yoga Slim 7 14IMH9 Ultra 7 155H/32GB/1TB/OfficeHS/Win11 (83CV001VVN)'),
 
 
 
 
 ('MSI', 'msi_gaming_gf63_thin_12ve_i5', 'Intel Core i5 Alder Lake - 12450H', 16, 'DDR4', '3200 MHz', 512, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4050 (6GB)', '15.6 inches', 'Full HD (1920 x 1080)', '144Hz', 18990000, 'Laptop MSI Gaming GF63 Thin 12VE i5 12450H/16GB/512GB/6GB RTX4050/144Hz/Win11 (460VN)'),
 
-('MSI', 'msi_gaming_katana_15_b13udxk_i7', 'Intel Core i7 Raptor Lake - 13620H', 16, 'DDR4', '5200 MHz', 1000, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 3050 (6GB)', '15.6 inches', 'Full HD (1920 x 1080)', '144Hz', 23990000, 'Laptop MSI Gaming Katana 15 B13UDXK i7 13620H/16GB/1TB/6GB RTX3050/144Hz/Win11 (2077VN)'),
+('MSI', 'msi_gaming_katana_15_b13udxk_i7', 'Intel Core i7 Raptor Lake - 13620H', 16, 'DDR4', '5200 MHz', 1024, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 3050 (6GB)', '15.6 inches', 'Full HD (1920 x 1080)', '144Hz', 23990000, 'Laptop MSI Gaming Katana 15 B13UDXK i7 13620H/16GB/1TB/6GB RTX3050/144Hz/Win11 (2077VN)'),
 
-('MSI', 'msi_gaming_stealth_14_ai_studio_a1vfg_ultra_7', 'Intel Core Ultra 7 Meteor Lake - 155H', 32, 'DDR5', '5600 MHz', 1000, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4060 (8GB)', '14 inches', '2.8K (2880 x 1800) - OLED', '120Hz', '48990000', 'Laptop MSI Gaming Stealth 14 AI Studio A1VFG Ultra 7 155H/32GB/1TB/8GB RTX4060/120Hz/Win11 (085VN)'),
+('MSI', 'msi_gaming_stealth_14_ai_studio_a1vfg_ultra_7', 'Intel Core Ultra 7 Meteor Lake - 155H', 32, 'DDR5', '5600 MHz', 1024, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4060 (8GB)', '14 inches', '2.8K (2880 x 1800) - OLED', '120Hz', '48990000', 'Laptop MSI Gaming Stealth 14 AI Studio A1VFG Ultra 7 155H/32GB/1TB/8GB RTX4060/120Hz/Win11 (085VN)'),
 
-('MSI', 'msi_gaming_sword_16_hx_b14vfkg_i7', 'Intel Core i7 Raptor Lake - 14700HX', 16, 'DDR5', '5600 MHz', 1000, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4060 (8GB)', '16.1 inches', 'Full HD (1920 x 1080)', '240Hz', 36990000, 'Laptop MSI Gaming Sword 16 HX B14VFKG i7 14700HX/16GB/1TB/8GB RTX4060/240Hz/Win11 (045VN)'),
+('MSI', 'msi_gaming_sword_16_hx_b14vfkg_i7', 'Intel Core i7 Raptor Lake - 14700HX', 16, 'DDR5', '5600 MHz', 1024, 'SSD NVMe PCIe', 'NVIDIA GeForce RTX 4060 (8GB)', '16.1 inches', 'Full HD (1920 x 1080)', '240Hz', 36990000, 'Laptop MSI Gaming Sword 16 HX B14VFKG i7 14700HX/16GB/1TB/8GB RTX4060/240Hz/Win11 (045VN)'),
 
 ('MSI', 'msi_modern_15_b12mo_i5', 'Intel Core i5 Alder Lake - 1235U', 16, 'DDR4', '3200 MHz', 512, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '15.6 inches', 'Full HD (1920 x 1080)', '60Hz', 12490000, 'Laptop MSI Modern 15 B12MO i5 1235U/16GB/512GB/Win11 (628VN)'),
 
-('MSI', 'msi_prestige_13_ai_evo_a1mg_ultra_7', 'Intel Core Ultra 7 Meteor Lake - 155H', 32, 'DDR5', '6400 MHz', 1000, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '13.4 inches', '2.8K (2880 x 1800) - OLED 16:10', '120Hz', 30990000, 'Laptop MSI Prestige 13 AI Evo A1MG Ultra 7 155H/32GB/1TB/Win11 (062VN)'),
+('MSI', 'msi_prestige_13_ai_evo_a1mg_ultra_7', 'Intel Core Ultra 7 Meteor Lake - 155H', 32, 'DDR5', '6400 MHz', 1024, 'SSD NVMe PCIe', 'Intel Iris Xe Graphics', '13.4 inches', '2.8K (2880 x 1800) - OLED 16:10', '120Hz', 30990000, 'Laptop MSI Prestige 13 AI Evo A1MG Ultra 7 155H/32GB/1TB/Win11 (062VN)'),
 
 
 ('Apple', 'apple_macbook_air_13_inch_m2', 'Apple M2', 8, 'Unified Memory', 'Hang khong cong bo', 256, 'SSD', 'Apple M2', '13.6 inches', '2560 x 1664', '60Hz', 24490000, 'Laptop Apple MacBook Air 13 inch M2 8GB/256GB (MLXY3SA/A)'),
@@ -92,7 +92,7 @@ VALUES
 
 ('Apple', 'apple_macbook_pro_14_inch_m3', 'Apple M3', 8, 'Unified Memory', 'Hang khong cong bo', 512, 'SSD', 'Apple M3', '14.2 inches', 'Liquid Retina XDR display (3024 x 1964)', '120 Hz', 38190000, 'Laptop Apple MacBook Pro 14 inch M3 8GB/512GB (MR7J3SA/A)'),
 
-('Apple', 'macbook_pro_14_inch_m2_pro_2023_12-core', 'Apple M2 Pro', 12, 'Unified Memory', 'Hang khong cong bo', 1000, 'SSD', 'Apple M2 Pro', '14.2 inches', 'Liquid Retina XDR display (3024 x 1964)', '120 Hz', 59690000, 'Laptop MacBook Pro 14 inch M2 Pro 2023 12-core CPU/16GB/1TB/19-core GPU (MPHJ3SA/A)'),
+('Apple', 'macbook_pro_14_inch_m2_pro_2023_12-core', 'Apple M2 Pro', 12, 'Unified Memory', 'Hang khong cong bo', 1024, 'SSD', 'Apple M2 Pro', '14.2 inches', 'Liquid Retina XDR display (3024 x 1964)', '120 Hz', 59690000, 'Laptop MacBook Pro 14 inch M2 Pro 2023 12-core CPU/16GB/1TB/19-core GPU (MPHJ3SA/A)'),
 
 ('Apple', 'apple_macbook_air_13_inch_m1', 'Apple M1', 8, 'Unified Memory', 'Hang khong cong bo', 256, 'SSD', 'Apple M1', '13.3 inches', '2560 x 1600', '60Hz', 18590000, 'Laptop Apple MacBook Air 13 inch M1 8GB/256GB (MGN63SA/A)'),
 
