44de81c1b1e3f201f67c41a86b49df192b1763ce
lamtriphucUTH <2251120436@ut.edu.vn>
2024-11-16 22:44:11 +0700

    fix

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,6 +1,5 @@
 <?php
 include_once("../includes/connect.php");
-include("get_revenue.php");
 if (isset($_SESSION['role']) && $_SESSION['role'] == 1) {
     header("Location: dashboard.php");
 } elseif (isset($_SESSION['role']) && $_SESSION['role'] == 0) {
